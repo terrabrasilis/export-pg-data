eb196d84aaec5460b7830e7f7fe195e313c1ecfd
andre-carvalho <afacarvalho@yahoo.com.br>
2023-01-03 14:19:04 -0300

    Improve output log messages

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -5,7 +5,9 @@ source /etc/environment
 DATA_DIR=$SHARED_DIR
 cd $INSTALL_PATH/
 
+LOG="${INSTALL_PATH}/docker-run.log"
+
 for CONF_FILE in `ls $DATA_DIR/config/ | awk {'print $1'}`
 do
-  /bin/bash ./get_databases.sh "${CONF_FILE}" "${DATA_DIR}" "${INSTALL_PATH}" &
+  /bin/bash ./get_databases.sh "${CONF_FILE}" "${DATA_DIR}" "${INSTALL_PATH}" >> ${LOG}
 done
--- src/get_databases.sh
+++ src/get_databases.sh
@@ -5,7 +5,7 @@
 CONF_FILE=${1}
 DATA_DIR=${2}
 INSTALL_PATH=${3}
-# load settings to the each SGDB host.
+echo "load settings to the each SGDB host."
 . "${INSTALL_PATH}/dbconf.sh" "$DATA_DIR/config/$CONF_FILE"
 
 OUTPUT_DATA=$DATA_DIR
@@ -16,17 +16,17 @@ PG_FILTER="--tuples-only -P format=unaligned"
 PG_QUERY="SELECT datname FROM pg_database WHERE NOT datistemplate AND datname ilike '${DB_NAME_LIKE}';"
 
 if [[ ! -d "${OUTPUT_DATA}/${1}" ]]; then
-  # creating output directory to put files
+  echo "creating output directory to put files"
   mkdir -p "${OUTPUT_DATA}/${1}"
 fi
 
 OUTPUT_DATA="${OUTPUT_DATA}/${1}"
 LOGFILE="${OUTPUT_DATA}/db_exports_${ACT_DATE}.log"
 
-echo "***** GET DATABASES FOR HOST ($ACT_DATE) *****" >> $LOGFILE
+echo "***** GET DATABASES FOR HOST ${host} *****" >> $LOGFILE
 for database in `echo -e $PG_QUERY |$PG_BIN/psql --dbname=postgres $PG_CON $PG_FILTER | sed /\eof/p | grep -v rows\) | awk {'print $1'}`
   do
     echo "Run exports to ${database} database at: "$(date '+%d-%m-%YT%H:%M:%S') >> $LOGFILE
     # export to files
-    . ./export_tables_to_file.sh
+    . ./export_tables_to_file.sh >> $LOGFILE
   done
