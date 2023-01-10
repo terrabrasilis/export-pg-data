f5cc08c2b72dbcf4ab9897585a4d943eca250db1
andre-carvalho <afacarvalho@yahoo.com.br>
2023-01-10 11:38:32 -0300

    Fix avoided file test

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -9,7 +9,7 @@ LOG="${INSTALL_PATH}/docker-run.log"
 for CONF_FILE in `ls $DATA_DIR/config/ | awk {'print $1'}`
 do
   # avoid if ${CONF_FILE} is exportconf
-  if [[ ! -f "${DATA_DIR}/config/${CONF_FILE}" ]];
+  if [[ ! "${CONF_FILE}" = "exportconf" ]];
   then
     /bin/bash ./get_databases.sh "${CONF_FILE}" "${DATA_DIR}" "${INSTALL_PATH}" >> ${LOG}
   fi;
